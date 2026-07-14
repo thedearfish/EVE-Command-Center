5ce4bcb7cdb3d75eb1be2d42416716cec23e4df1
thedearfish <thedearfish@gmail.com>
2026-07-14 18:41:32 +0300

    Log tray menu and command execution

--- src/EveCommandCenter.App/Shell/TrayIconService.cs
+++ src/EveCommandCenter.App/Shell/TrayIconService.cs
@@ -1,4 +1,5 @@
 using System.Drawing;
+using EveCommandCenter.Application.Diagnostics;
 using Forms = System.Windows.Forms;
 
 namespace EveCommandCenter.App.Shell;
@@ -19,15 +20,17 @@ public sealed class TrayIconService : IDisposable
         ArgumentNullException.ThrowIfNull(exitApplication);
 
         contextMenu = new Forms.ContextMenuStrip();
+        contextMenu.Opening += (_, _) => AppLog.Information("Tray", "Tray menu opened.");
+        contextMenu.Closed += (_, _) => AppLog.Debug("Tray", "Tray menu closed.");
 
         var settingsItem = new Forms.ToolStripMenuItem("Settings...");
-        settingsItem.Click += (_, _) => showSettings();
+        settingsItem.Click += (_, _) => ExecuteCommand("Settings", showSettings);
 
         var togglePreviewsItem = new Forms.ToolStripMenuItem("Show / hide previews");
-        togglePreviewsItem.Click += (_, _) => togglePreviews();
+        togglePreviewsItem.Click += (_, _) => ExecuteCommand("Show / hide previews", togglePreviews);
 
         var exitItem = new Forms.ToolStripMenuItem("Exit");
-        exitItem.Click += (_, _) => exitApplication();
+        exitItem.Click += (_, _) => ExecuteCommand("Exit", exitApplication);
 
         contextMenu.Items.Add(settingsItem);
         contextMenu.Items.Add(togglePreviewsItem);
@@ -42,7 +45,8 @@ public sealed class TrayIconService : IDisposable
             Visible = true,
         };
 
-        notifyIcon.DoubleClick += (_, _) => showSettings();
+        notifyIcon.DoubleClick += (_, _) => ExecuteCommand("Settings (double-click)", showSettings);
+        AppLog.Information("Tray", "System tray icon created.");
     }
 
     public void Dispose()
@@ -53,8 +57,30 @@ public sealed class TrayIconService : IDisposable
         }
 
         disposed = true;
+        AppLog.Information("Tray", "Disposing system tray icon.");
         notifyIcon.Visible = false;
         notifyIcon.Dispose();
         contextMenu.Dispose();
+    }
+
+    private static void ExecuteCommand(string commandName, Action action)
+    {
+        AppLog.SetCurrentOperation($"tray command: {commandName}");
+        AppLog.Information("Tray", $"Tray command selected: {commandName}.");
+
+        try
+        {
+            action();
+            AppLog.Information("Tray", $"Tray command completed: {commandName}.");
+        }
+        catch (Exception exception)
+        {
+            AppLog.Error("Tray", $"Tray command failed: {commandName}.", exception);
+            throw;
+        }
+        finally
+        {
+            AppLog.SetCurrentOperation("idle");
+        }
     }
 }
